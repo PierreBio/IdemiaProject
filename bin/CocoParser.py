762163beae14ea753a724463966714710e2e1b1d
JPGodTier <145616322+JPGodTier@users.noreply.github.com>
2023-11-30 16:16:36 +0100

    [CocoParser]: Fixed target calculating issues, changed pedestrian id def

--- bin/CocoParser.py
+++ bin/CocoParser.py
@@ -21,18 +21,23 @@ def main(data_path):
             for img_ann in img_anns:
                 img_kps = img_ann["keypoints"]
 
-                # Checking if feets are visible (last & 3rd last elements of keypoints)
+                # Feet Visibility Check
                 if img_kps[-1] != 0 and img_kps[-4] != 0:
+                    # % of visible keypoints check
                     if are_keypoints_visible(img_kps[:-6], 70):
-                        print("accepted")
+                        print(f"ACCEPTED: Img {img_id}")
+
+                        target = [(img_kps[-3] + img_kps[-6]) / 2, (img_kps[-5] + img_kps[-2]) / 2]
+                        saved_data.append(prepare_data(img_id, img_ann["id"], img_kps[:-6], target))
+
+                        # Displaying Img for debug purpose
                         # display_img(coco, img, img_anns)
-                        target = [(img_kps[-3] + img_kps[-6]) / 2, (img_kps[-3] + img_kps[-6]) / 2]
-                        saved_data.append(prepare_data(img_id, img_kps[:-6], target))
                         break
                     else:
                         print(f"REJECTED: Not enough keypoints visble image {img_id}")
                 else:
                     print(f"REJECTED: Feets no visible for image {img_id}")
+                    # Displaying Img for debug purpose
                     # display_img(coco, img, img_anns)
 
             save_to_csv("output", saved_data)
@@ -41,18 +46,23 @@ def main(data_path):
         exit(-1)
 
 
+# -----------------------------------------------------------------------------
+# display_img
+# -----------------------------------------------------------------------------
 def display_img(coco_db, image, annotations):
     # Display img from url
     image = io.imread(image["coco_url"])
-
     plt.imshow(image)
+
     # Display kps
     coco_db.showAnns(annotations)
 
     plt.axis('off')
     plt.show()
 
-
+# -----------------------------------------------------------------------------
+# are_keypoints_visible
+# -----------------------------------------------------------------------------
 def are_keypoints_visible(keypoint_list, percentage_threshold):
     # Extracting visibility values from keypoints
     v_values = keypoint_list[2::3]
@@ -68,13 +78,19 @@ def are_keypoints_visible(keypoint_list, percentage_threshold):
     return visible_percentage >= percentage_threshold
 
 
+# -----------------------------------------------------------------------------
+# prepare_data
+# -----------------------------------------------------------------------------
 def prepare_data(*args):
     return [arg for arg in args]
 
 
+# -----------------------------------------------------------------------------
+# save_to_csv
+# -----------------------------------------------------------------------------
 def save_to_csv(file_name, data_list):
     # Define file  headers
-    headers = ["Img_Id", "Keypoints", "Target"]
+    headers = ["Img_Id", "Pedestrian_id", "Keypoints", "Target"]
 
     with open(file_name, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
@@ -91,7 +107,9 @@ def startup_msg():
     print("Starting COCO Parser...")
 
 
+# TODO : occultation, data augmentation (more kps bottom/ more kps top)
+# TODO : check for deep learning models (pytorch)
 if __name__ == "__main__":
     startup_msg()
-    ann_file_path = os.path.join("../data/annotations_trainval2017/annotations/person_keypoints_val2017.json")
+    ann_file_path = os.path.join("../Coco/annotations_trainval2017/person_keypoints_val2017.json")
     main(ann_file_path)
