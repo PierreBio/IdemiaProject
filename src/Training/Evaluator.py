439d4edfd0903248e2c9164a68c23ed71bf0e123
JPGodTier <145616322+JPGodTier@users.noreply.github.com>
2024-02-29 15:01:19 +0100

    [Model]: Model updated, now displaying plot loss for train and validation, visualization file available, OCHumans data added

--- src/Training/Evaluator.py
+++ src/Training/Evaluator.py
@@ -3,18 +3,21 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 
-def evaluate_model(model, dataloader, device):
+def evaluate_model(model, dataloader, device, loss_function):
     model.eval()
+    losses = []
     y_pred, y_true = [], []
 
     with torch.no_grad():
         for _, _, inputs, targets in dataloader:
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = model(inputs)
+            loss = loss_function(outputs, targets)
+            losses.append(loss.item())
 
             y_pred.extend(outputs.cpu().numpy())
             y_true.extend(targets.cpu().numpy())
 
     root_mse = sqrt(mean_squared_error(y_true, y_pred))
     print(f"Evaluation RMSE: {root_mse:.4f}")
-    return root_mse
+    return root_mse, losses
